4c647980c9dd253e08504d7ddc49a4555951bbe3
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-05-31 12:53:13 -0400

    Changed homepage layout

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,7 +8,8 @@ export default function Home() {
     return <div className="" >
     <div style={{position:'relative'}}>
 
-    <h1 style={{position:'absolute', top:'8px', right:'16px', fontSize:'50px'}} className="float-right" >Travel Needs Adventure</h1>
+    <h2 style={{position:'absolute', top:'8px', right:'16px', fontSize:'40px'}} className="float-right" >WHERE TRAVEL MEETS ADVENTURE</h2>
+    <h1 style={{position: 'absolute', top: '50%', left:'50%',transform: 'translate(-50%, -50%)', fontSize:'70px' }}>H O S T E L L E</h1>
         <img src="https://images.unsplash.com/photo-1471506480208-91b3a4cc78be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80" 
         alt ="long road between hills" 
         style={{width:'100%',height:'100%'}}/>
